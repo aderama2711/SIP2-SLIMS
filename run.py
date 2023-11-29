7dd7af82e8977a9f0d604183cd09bd119e63e794
Ade Aditya Ramadha <ade.ramadha@gmail.com>
2023-11-29 13:32:55 +0700

    Fix logging

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ import logging
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 
+logger = logging.getLogger("nameOfTheLogger")
+ConsoleOutputHandler = logging.StreamHandler()
+
+
+
 HOST = "127.0.0.1"  # The IP Address of Translator SIP 
 PORT = 6001  # The port used by Translator SIP
 
@@ -31,11 +36,11 @@ while True:
             s.listen()
             conn, addr = s.accept()
             with conn:
-                logging.info(f"Connected by {addr}")
+                logger.info(f"Connected by {addr}")
                 while True:
                     data = conn.recv(1024)
                     string = data.decode('utf-8')
-                    logging.info(string)
+                    logger.info(string)
                     if not data:
                         break
 
@@ -46,12 +51,12 @@ while True:
 
                     # SC registration
                     if string[0:2] == "99":
-                        logging.info("SC registration")
+                        logger.info("SC registration")
                         resp = bytes("98YYYNNN500   003"+gettime()+"2.00AO"+library_name+"|BXNYYNYNNYNNYNNNNN"+"\r", 'utf-8')
                     
                     # item information
                     if string[0:2] == "17":
-                        logging.info("Item Information")
+                        logger.info("Item Information")
                         # get book ID
                         item_id = string.split("AB")[1].split("|")[0]
 
@@ -103,14 +108,14 @@ while True:
 
                     # patron end session
                     elif string[0:2] == "35":
-                        logging.info("Patron End Session")
+                        logger.info("Patron End Session")
                         # get user ID
                         user_id = string.split("AA")[1].split("|")[0]
                         resp = bytes("36Y"+gettime()+"|AO"+library_name+"|AA"+user_id+"\r", 'utf-8')
                     
                     # patron status
                     elif string[0:2] == "23":
-                        logging.info("Patron Status")
+                        logger.info("Patron Status")
                         # get user ID
                         user_id = string.split("AA")[1].split("|")[0]
 
@@ -138,7 +143,7 @@ while True:
 
                     # patron information
                     elif string[0:2] == "63":
-                        logging.info("Patron Information")
+                        logger.info("Patron Information")
                         # get user ID
                         user_id = string.split("AA")[1].split("|")[0]
 
@@ -184,13 +189,13 @@ while True:
 
                     # check out
                     elif string[0:2] == "11":
-                        logging.info("Checkout")
+                        logger.info("Checkout")
                         # get user id and item id
                         user_id = string.split("AA")[1].split("|")[0]
                         item_id = string.split("AB")[1].split("|")[0]
 
                         mycursor = mydb.cursor()
-                        mycursor.execute("SELECT * from loan where is_lent=1 and is_return=0 AND TO_DAYS(due_date) < TO_DAYS(NOW()) AND member_id='"+user_id+"'")
+                        mycursor.execute("SELECT * from loan where is_lent=1 and is_return=0 AND TO_DAYS(due_date) < TO_DAYS(NOW()) AND member_id='"+user_id+"'")
                         myresult = mycursor.fetchall()
 
                         if len(myresult) != 0:
@@ -262,8 +267,8 @@ while True:
 
                                             mydb.commit()
 
-                                            logging.info(mycursor.rowcount, "record inserted.")
-                                            logging.info(mycursor._warnings)
+                                            logger.info(mycursor.rowcount, "record inserted.")
+                                            logger.info(mycursor._warnings)
 
 
                                             if slims_version == 9:
@@ -275,8 +280,8 @@ while True:
 
                                                 mydb.commit()
 
-                                                logging.info(mycursor.rowcount, "record inserted.")
-                                                logging.info(mycursor._warnings)
+                                                logger.info(mycursor.rowcount, "record inserted.")
+                                                logger.info(mycursor._warnings)
                                     else:
                                         resp = bytes("121NNY"+gettime()+"AO"+library_name+"|AA"+user_id+"|AH"+str((datetime.datetime.now() + datetime.timedelta(days=loan_periode)).strftime('%Y-%m-%d'))+"|AB"+item_id+"|AJ"+title+"|AFBUKU BERHASIL DIPINJAM"+"\r", 'utf-8')
                                             
@@ -288,8 +293,8 @@ while True:
 
                                         mydb.commit()
 
-                                        logging.info(mycursor.rowcount, "record inserted.")
-                                        logging.info(mycursor._warnings)
+                                        logger.info(mycursor.rowcount, "record inserted.")
+                                        logger.info(mycursor._warnings)
 
                                         if slims_version == 9:
                                             # insert to log
@@ -300,16 +305,16 @@ while True:
 
                                             mydb.commit()
 
-                                            logging.info(mycursor.rowcount, "record inserted.")
-                                            logging.info(mycursor._warnings)
+                                            logger.info(mycursor.rowcount, "record inserted.")
+                                            logger.info(mycursor._warnings)
                             
                     # check in
                     elif string[0:2] == "09":
-                        logging.info("Checkin")
+                        logger.info("Checkin")
                         returnY = string[3:7]
                         returnM = string[7:9]
                         returnD = string[9:11]
-                        logging.info(returnY, returnM, returnD)
+                        logger.info(returnY, returnM, returnD)
                         item_id = string.split("AB")[1].split("|")[0]
 
                         mycursor = mydb.cursor()
@@ -354,17 +359,17 @@ while True:
 
                                     mydb.commit()
 
-                                    logging.info(mycursor.rowcount, "record inserted.")
-                                    logging.info(mycursor._warnings)
+                                    logger.info(mycursor.rowcount, "record inserted.")
+                                    logger.info(mycursor._warnings)
                             
                             else:
                                 resp = bytes("100NNY"+gettime()+"AO"+library_name+"|AB"+item_id+"|AQ|AJ"+title+"|AFBUKU BELUM DIPINJAM"+"\r", 'utf-8')
 
 
-                    logging.info(resp)
+                    logger.info(resp)
                     conn.sendall(resp)
     except Exception as error:
-        logging.warning(traceback.format_exc())
+        logger.warning(traceback.format_exc())
 
 # id = "B00001"
 
